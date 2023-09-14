fbcc62f15a2de890c3f6604b0c82b4006c5f03ff
norbikj2002 <kesetjoaca@gmail.com>
2023-09-14 22:47:43 +0300

    start button

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 import '../static/styles/page_styles/Index.css';
 
 const Index = () => {
   return (
     <div className='Index'>
-
+      <Button
+        variant='outlined'
+        color='primary'
+      >
+        Guess the Number
+      </Button>
     </div>
   );
 };
