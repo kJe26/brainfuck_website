f1c7b3ad644c7d41e5eddef5e1ca09e29949f26a
norbikj2002 <kesetjoaca@gmail.com>
2023-09-14 22:25:30 +0300

    home page init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+// custom components
+import { 
+  Route, 
+  BrowserRouter as Router, 
+  Routes 
+} from 'react-router-dom';
+import Index from './pages/Index';
+
 // styles
 import './static/styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route index element={<Index/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../static/styles/page_styles/Index.css';
+
+const Index = () => {
+  return (
+    <div className='Index'>
+
+    </div>
+  );
+};
+
+export default Index;
